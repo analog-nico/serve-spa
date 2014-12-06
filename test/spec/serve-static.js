42a8fc26dc19550d5a66efaa00807ef265ae8f33
analog-nico <nicolai.kamenzky@testrails.org>
2014-12-05 23:03:37 -0500

    Basic serve static tests

--- test/spec/serve-static.js
+++ test/spec/serve-static.js
@@ -1,9 +1,64 @@
 'use strict';
 
+var express = require('express');
+var http = require('http');
+var serveSpa = require('../../lib/index.js');
+var path = require('path');
+var rp = require('request-promise');
+
+
 // TODO: Check if redirect to / works
 
-describe('...', function () {
+describe('Regarding serving static files, Serve-SPA', function () {
 
-    it('...');
+    var server;
+
+    before(function (done) {
+        var app = express();
+        serveSpa(app, path.join(__dirname, '../fixtures/serve-static/'));
+        server = http.createServer(app);
+        server.listen(4000, function () { done(); });
+    });
+
+    after(function () {
+        server.close();
+    });
+
+
+    it('should serve files in the root directory', function () {
+
+        return rp('http://localhost:4000/test.json')
+            .then(function (body) {
+                expect(body).to.equal('{ "loc": "root" }');
+            });
+
+    });
+
+    it('should serve files in a subdirectory', function () {
+
+        return rp('http://localhost:4000/sub1/test.json')
+            .then(function (body) {
+                expect(body).to.equal('{ "loc": "sub1" }');
+            });
+
+    });
+
+    it('should return a 404 for index.html', function () {
+
+        return rp({ uri: 'http://localhost:4000/index.htmlt', simple: false, resolveWithFullResponse: true })
+            .then(function (response) {
+                expect(response.statusCode).to.equal(404);
+            });
+
+    });
+
+    it('should return a 404 for preproc.js', function () {
+
+        return rp({ uri: 'http://localhost:4000/preproc.js', simple: false, resolveWithFullResponse: true })
+            .then(function (response) {
+                expect(response.statusCode).to.equal(404);
+            });
+
+    });
 
 });
